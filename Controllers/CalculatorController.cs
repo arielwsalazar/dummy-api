4559aad7fea1e6b6a73529c645e809a5055b2a44
aws <arielwsalazar@yahoo.com.ar>
2022-02-23 00:37:37 -0300

    first commit

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dummyApi.Controllers
+{
+    [ApiController]
+    [Route("calculator")]
+    public class CalculatorController:ControllerBase 
+    {
+        [Route("Add")]
+        [HttpGet]
+        public JsonResult Add([FromQuery(Name ="Value1")] int val1, [FromQuery(Name = "Value2")] int val2)
+        {
+            Dictionary<string, int> result = new Dictionary<string, int>();
+            result.Add("result", val1 + val2);
+            return new JsonResult(result);
+        }
+    }
+}
